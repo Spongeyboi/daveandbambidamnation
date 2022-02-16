26946884e2149593f0a1dad3a1b4932bf30812ea
Spongey <71215126+Spongeyboi@users.noreply.github.com>
2022-02-16 10:46:13 -0500

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -139,6 +139,23 @@ class Character extends FlxSprite
 				addOffset("singDOWN", 0, -30);
 
 				playAnim('idle');
+			case 'dave':
+				// DAD ANIMATION LOADING CODE
+				tex = Paths.getSparrowAtlas('DAVE');
+				frames = tex;
+				animation.addByPrefix('idle', 'John idle dance', 24);
+				animation.addByPrefix('singUP', 'John Sing Note UP', 24);
+				animation.addByPrefix('singRIGHT', 'John Sing Note RIGHT', 24);
+				animation.addByPrefix('singDOWN', 'John Sing Note DOWN', 24);
+				animation.addByPrefix('singLEFT', 'John Sing Note LEFT', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, -10);
+				addOffset("singLEFT", -10, 10);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
 			case 'spooky':
 				tex = Paths.getSparrowAtlas('spooky_kids_assets');
 				frames = tex;
