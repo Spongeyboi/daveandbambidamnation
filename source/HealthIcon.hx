93ce438d09ed013001257c6cd48dc048ff848704
Spongey <71215126+Spongeyboi@users.noreply.github.com>
2022-02-16 11:26:37 -0500

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -35,6 +35,7 @@ class HealthIcon extends FlxSprite
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
 		animation.add('dave', [24, 25], 0, false, isPlayer);
+		animation.add('dumbass-dave', [24, 25], 0, false, isPlayer);
 		animation.play(char);
 		scrollFactor.set();
 	}
